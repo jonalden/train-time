f988ac39c5c8a458790e3b0b4a7d11a29c6112e4
jon alden <jondevalden@gmail.com>
2019-08-14 16:01:59 -0500

    j

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,16 +1,23 @@
+
+function getTrainInfo(cb) {
+    localforage.getItem("trainSchedules").then(function (result) {
+        cb(result || []); //{name, score}
+    });
+}
+
+function setTrainInfo(newTrainInfo, cb) {
+    localforage.setItem("trainSchedules", newTrainInfo).then(cb);
+};
+
 document
     .getElementById("submitButton")
-    .addEventListener("click", function () {
-
-        // const trainName = document.getElementById("trainName").value.trim();
-        // const destination = document.getElementById("destinantion").value.trim();
-        // const firstTrain = document.getElementById("firstTrain").value.trim();
-        // const frequency = document.getElementById("frequency").value.trim();
-
-        const trainName = "trainName"
-        const destination = "destination"
-        const firstTrain = "firstTrain"
-        const frequency = "frequency"
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        const trainName = document.getElementById("trainName").value.trim();
+        const destination = document.getElementById("destinantion").value.trim();
+        const firstTrain = document.getElementById("firstTrain").value.trim();
+        const frequency = document.getElementById("frequency").value.trim();
+        
 
         const trainData = {
             trainName: trainName,
@@ -18,19 +25,35 @@ document
             firstTrain: firstTrain,
             frequency: frequency
         }
+        console.log(trainData);
 
-       
+        getTrainInfo(function (result) {
+            let newArray = result;
 
-        const myFunctionSet = localforage.setItem("trainName", "trainName").then(function (value) {
-            console.log(value);
-        });
-        
-        const myFunctionGet = localforage.getItem("hello").then(function (value) {
-            console.log(value);
+            newArray.push(trainData);
+
+            setTrainInfo(newArray, function () {
+                console.log(newArray);
+            })
         })
-        
-        myFunctionSet(trainData);
-        console.log(myFunctionSet);
-        // myFunctionGet();
-    })
+    });
+
+const table = document.querySelector("table");
+
+setInterval(function () {
+    localforage
+        .getTrainInfo("trainSchedules")
+        .then(function (result) {
+            if(!result) {
+            result = [];
+            }
+            table.innerHTML = "";
+            for(let i = 0; i < result.length; i++) {
+                
+            }
+        })
+}, 1000);
+
+
+
 
